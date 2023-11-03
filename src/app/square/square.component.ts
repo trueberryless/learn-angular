2701249cbce5ac880e4da24e5e9ee9b4c0d47557
trueberryless <trueberryless+github@gmail.com>
2023-11-03 15:06:26 +0100

    Initial commit

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: ` <button>{{ value }}</button> `,
+  styles: [
+    `
+      button {
+        width: 100%;
+        height: 100%;
+        font-size: 100px;
+      }
+    `,
+  ],
+})
+export class SquareComponent {
+  @Input() value: 'X' | 'O' | undefined;
+}
